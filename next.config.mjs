f8d6d6d91cbde089727249758a3be272258ae5b6
MukulWaval <mukulwaval2000@gmail.com>
2025-02-06 10:17:27 +0530

    Update Next.js configuration for static export and GitHub Pages compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,17 @@ import mdx from "@next/mdx";
 
 const withMDX = mdx({
   extension: /\.mdx?$/,
-  options: {}
+  options: {},
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+  output: "export", // Enables static export
+  assetPrefix: process.env.GITHUB_PAGES ? "/page/" : "",
+  images: {
+    unoptimized: true, // GitHub Pages does not support Next.js Image Optimization
+  },
 };
 
 export default withMDX(nextConfig);
